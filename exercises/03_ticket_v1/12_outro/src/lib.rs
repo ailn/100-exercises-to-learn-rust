82aa2f633cd4be208f074dcc94f95e510cb31bfa
Andrei Ilnitskii <andrey.ilnitsky@gmail.com>
2024-09-27 20:37:22 -0700

    03_12 complete

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,74 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::verify_product_name(&product_name);
+        Self::verify_quantity(&quantity);
+        Self::verify_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn set_product_name(&mut self, new_name: String) {
+        Self::verify_product_name(&new_name);
+        self.product_name = new_name;
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn set_quantity(&mut self, new_qty: u32) {
+        Self::verify_quantity(&new_qty);
+        self.quantity = new_qty;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, new_price: u32) {
+        Self::verify_unit_price(&new_price);
+        self.unit_price = new_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        self.unit_price * self.quantity
+    }
+
+    fn verify_product_name(name: &String) {
+        if name.is_empty() {
+            panic!("The product name can't be empty");
+        }
+        if name.len() > 300 {
+            panic!("The product name can't be longer than 300 bytes");
+        }
+    }
+
+    fn verify_unit_price(price: &u32) {
+        if *price == 0 {
+            panic!("Unit price cannot be zero");
+        }
+    }
+
+    fn verify_quantity(quantity: &u32) {
+        if *quantity == 0 {
+            panic!("Quantity cannot be zero");
+        }
+    }
+}
