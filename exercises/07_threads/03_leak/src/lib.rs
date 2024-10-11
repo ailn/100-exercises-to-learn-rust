6136f51de60251ed1942c0bc508cea08849cd79e
Andrei Ilnitskii <andrey.ilnitsky@gmail.com>
2024-10-10 23:35:23 -0700

    07_03 complete

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,15 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let slice: &'static mut [i32] = v.leak();
+
+    let mid = slice.len() / 2;
+    let (slice1, slice2) = slice.split_at(mid);
+
+    let handle1 = thread::spawn(|| slice1.iter().sum::<i32>());
+    let handle2 = thread::spawn(|| slice2.iter().sum::<i32>());
+
+    handle1.join().unwrap() + handle2.join().unwrap()
 }
 
 #[cfg(test)]
